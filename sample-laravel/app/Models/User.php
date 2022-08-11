5aff8e629fe4fddd7da1e2d095f45cc439d38d3d
Lê Thị Hoa <nanggat2018@gmail.com>
2022-08-11 10:00:02 +0700

    Laravel basic2 Update_7

--- sample-laravel/app/Models/User.php
+++ sample-laravel/app/Models/User.php
@@ -2,15 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\Request;
 
 class User extends Authenticatable
 {
@@ -31,7 +24,9 @@ class User extends Authenticatable
 
     public function listUser() 
     {
-        return self::select('mail_address', 'password', 'name', 'address', 'phone',)->orderBy('mail_address', 'ASC')->paginate(20);
+        return self::select('mail_address', 'password', 'name', 'address', 'phone',)
+            ->orderBy('mail_address', 'ASC')
+            ->paginate(20);
     }
 
     public function createUser($request) 
